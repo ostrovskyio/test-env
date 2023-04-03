54dfebe277f81d566f6fa2edbdbb77524abf981b
Viktor Kondolovskiy <viktor.kondolovskyi@theopentag.com>
2023-04-03 18:26:02 +0300

    cleamup

--- testengine/TestManager.js
+++ testengine/TestManager.js
@@ -6,7 +6,6 @@
     jasmineEnv.addReporter(reporter);
 
     window.initiateTest = function() {
-        console.log('test initiated');
         var testPanel = document.createElement('div');
         testPanel.setAttribute('id', 'test-panel');
         document.querySelector(".js-container").appendChild(testPanel);
