0423dfa11d62abc4a9a694a49025beb6ae2fd5bd
ostrovskyio <ostrovskyoleg2001@gmail.com>
2023-04-10 22:17:52 +0300

    TEST

--- src/index.js
+++ src/index.js
@@ -1,7 +1,77 @@
-const btn = document.querySelector('.js-button');
+//LESSON 23
 
-function onClick(e) {
-    console.log('click', e.target);
-}   
+const numbers = [
+    10,
+    21,
+    32,
+    43,
+    54,
+    65,
+    76,
+    87,
+    98,
+    109];
 
-btn.addEventListener('click', onClick);
+    let sum = 0;
+
+    for (let i=0; i < numbers.length; i++) {
+        if (typeof numbers[i] === "number") {
+            sum = sum + numbers[i];
+        }
+    }
+    console.log("Sum of numbers:" + sum);
+
+    let min = Infinity;
+    let max = -Infinity;
+
+    for (let i=0; i < numbers.length; i++) {
+        if (typeof numbers[i] === "number"){
+            if (numbers[i] < min) {
+            min = numbers[i];
+            }
+            if (numbers[i] > max) {
+            max = numbers[i];
+            }
+        }
+    }
+    console.log("Minimal:" + min);
+    console.log("Maximum:" + max);
+
+    let tree = "";
+    for (let i = 0; i < 5; i++) {
+        tree += "#";
+        console.log(tree);
+      }
+
+//LESSON 26
+function Accumulator(initialValue) {
+    this.value = initialValue;
+    this.increment = function() {
+      this.value++;
+    }
+    
+    this.decrement = function() {
+      this.value--;
+    }
+    return this.value
+  }
+
+const accumulator = new Accumulator(5);
+accumulator.increment();
+accumulator.decrement();
+console.log(accumulator.value);
+
+
+function CancelableAccumulator(initialValue) {
+    Accumulator.call(this, initialValue);
+    this.clear = function() {
+        this.value = initialValue;
+    }
+}
+const can = new CancelableAccumulator(3);
+can.increment();
+can.clear();
+can.decrement();
+console.log(can.value);
+
+
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,48 @@
 
-describe("Check user event", () => {
-    const desc = "<p>Check if button was clicked</p>";
+describe("", () => {
+    const desc = "<p>Check sum of numbers</p>";
 
     describe(desc, () => {
-        it("Check if button was clicked", () => {
-            spyOn(console, 'log');
-            onClick({
-                target: 'button'
-            });
-            expect(console.log).toHaveBeenCalled();
+        it("Should return 595", () => {
+            const result = sum
+            expect(result).toBe(595)
         });
     });
 });
+
+describe("", () => {
+    const desc = "<p>Find min and max</p>";
+
+    describe(desc, () => {
+        it("Should return 10 and 109", () => {
+            const resultmin = min
+            const resultmax = max
+            expect(resultmin).toBe(10)
+            expect(resultmax).toBe(109)
+        });
+    });
+});
+
+describe("LESSON 23", () => {
+    const desc = "<p>Build tree</p>";
+
+    describe(desc, () => {
+        it("Should return tree", () => {
+            const resulttree = tree
+            expect(resulttree).toBe(tree += "#")
+        });
+    });
+});
+
+describe("", () => {
+    const desc = "<p>Accumulator</p>";
+
+    describe(desc, () => {
+        it("Should return tree", () => {
+            const resulttree = tree
+            expect(resulttree).toBe(tree += "#")
+        });
+    });
+});
+
+
