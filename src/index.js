f4f9df773f19334b7324295215b07ec0031fff35
ostrovskyio <ostrovskyoleg2001@gmail.com>
2023-04-10 23:23:44 +0300

    TEST

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ const numbers = [
       }
 
 //LESSON 26
+
 function Accumulator(initialValue) {
     this.value = initialValue;
     this.increment = function() {
@@ -73,5 +74,32 @@ can.increment();
 can.clear();
 can.decrement();
 console.log(can.value);
+
+//LESSON 28
+
+function zoomIn(img) {
+    if (img.style.width === '150%') {
+      zoomOut(img);
+      return;
+    }
+    img.style.transition = 'all 0.5s ease-in-out';
+    img.style.width = '150%';
+    img.style.height = '150%';
+  }
+  
+  function zoomOut(img) {
+    img.style.transition = 'all 0.5s ease-in-out';
+    img.style.width = '100%';
+    img.style.height = '100%';
+  }
+
+  document.addEventListener('keydown', function(event) {
+    var key = event.key.toLowerCase();
+    var element = document.getElementById(key);
+    if (element) {
+      element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    }
+  });
+
 
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,12 @@
 
-describe("", () => {
-    const desc = "<p>Check sum of numbers</p>";
-
-    describe(desc, () => {
+describe("Sum L23", () => {
         it("Should return 595", () => {
             const result = sum
             expect(result).toBe(595)
         });
     });
-});
 
-describe("", () => {
-    const desc = "<p>Find min and max</p>";
-
-    describe(desc, () => {
+describe("MIN & MAX L23", () => {
         it("Should return 10 and 109", () => {
             const resultmin = min
             const resultmax = max
@@ -21,28 +14,105 @@ describe("", () => {
             expect(resultmax).toBe(109)
         });
     });
-});
 
-describe("LESSON 23", () => {
-    const desc = "<p>Build tree</p>";
 
-    describe(desc, () => {
-        it("Should return tree", () => {
-            const resulttree = tree
-            expect(resulttree).toBe(tree += "#")
-        });
+describe("Tree L23", () => {
+    let treeOutput;
+  
+    beforeEach(() => {
+      treeOutput = "";
     });
-});
-
-describe("", () => {
-    const desc = "<p>Accumulator</p>";
-
-    describe(desc, () => {
-        it("Should return tree", () => {
-            const resulttree = tree
-            expect(resulttree).toBe(tree += "#")
-        });
+  
+    it("Should print tree", () => {
+      for (let i = 0; i < 5; i++) {
+        treeOutput += "#";
+        console.log(treeOutput);
+      }
+      expect(treeOutput).toEqual("#####");
     });
-});
+  
+  });
+
+describe("Accumulator L26", () => {
+    let accumulator;
+    let can;
+  
+    beforeEach(() => {
+      accumulator = new Accumulator(5);
+      can = new CancelableAccumulator(3);
+    });
+  
+    it("Should increment and decrement", () => {
+      accumulator.increment();
+      expect(accumulator.value).toEqual(6);
+      accumulator.decrement();
+      expect(accumulator.value).toEqual(5);
+    });
+  
+    it("Should clear", () => {
+      can.increment();
+      expect(can.value).toEqual(4);
+      can.clear();
+      expect(can.value).toEqual(3);
+      can.decrement();
+      expect(can.value).toEqual(2);
+    });
+  
+  });
+
+describe("Zoom L28", () => {
+    let img;
+  
+    beforeEach(() => {
+      img = document.createElement("img");
+      img.style.width = "100%";
+      img.style.height = "100%";
+    });
+  
+    afterEach(() => {
+      img = null;
+    });
+  
+    it("Should zoom in", () => {
+      zoomIn(img);
+  
+      expect(img.style.width).toEqual("150%");
+      expect(img.style.height).toEqual("150%");
+    });
+  
+    it("Should zoom out", () => {
+      zoomOut(img);
+  
+      expect(img.style.width).toEqual("100%");
+      expect(img.style.height).toEqual("100%");
+    });
+  
+  });
+
+  describe("Scroll L28", () => {
+    let event;
+  
+    beforeEach(() => {
+      event = new KeyboardEvent("keydown", { key: "a" });
+    });
+  
+    afterEach(() => {
+      event = null;
+    });
+  
+    it("Should scroll to 'a'", () => {
+      const element = document.createElement("div");
+      elementA.id = "a";
+      document.body.appendChild(elementA);
+      document.dispatchEvent(event);
+  
+      expect(window.scrollY).toBeGreaterThan(0);
+      expect(elementA.getBoundingClientRect().top).toBeCloseTo(0, 1);
+  
+      elementA.remove();
+    });
+  });
+
+
 
 
